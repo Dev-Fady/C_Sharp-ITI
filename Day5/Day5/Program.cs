f315e287f8df6bf72df84be89aa7493998630e14
Dev-Fady <fadymobiledev@gmail.com>
2025-07-17 15:13:04 +0300

    add commont IEnumerable

--- Day5/Day5/Program.cs
+++ Day5/Day5/Program.cs
@@ -5,38 +5,42 @@ namespace Day5
     {
         static void Main(string[] args)
         {
-           MyList myList = new MyList();
-            myList.Add(1);
-            myList.Add(2);
-            myList.Add(3);
-            myList.Add(4);
-            myList.Add(5);
+            #region IEnumerable
+            //MyList myList = new MyList();
+            // myList.Add(1);
+            // myList.Add(2);
+            // myList.Add(3);
+            // myList.Add(4);
+            // myList.Add(5);
 
-            for (int i = 0; i < myList.Count; i++)
-            {
-                Console.WriteLine(myList.GetByIndex(i));
-            }
+            // for (int i = 0; i < myList.Count; i++)
+            // {
+            //     Console.WriteLine(myList.GetByIndex(i));
+            // }
 
-            /*    int[] arr = {1, 2, 3, 4, 50, 6};
-                foreach (var item in arr)
-                {
-                    Console.WriteLine($"item is {item}");
-                }*/
+            // /*    int[] arr = {1, 2, 3, 4, 50, 6};
+            //     foreach (var item in arr)
+            //     {
+            //         Console.WriteLine($"item is {item}");
+            //     }*/
 
-            #region error becuase myList is privite
-            /*
-                foreach (var item in myList)
-                {
-                    Console.WriteLine($"item is {item}");
-                }
-                */
+            // #region error becuase myList is privite
+            // /*
+            //     foreach (var item in myList)
+            //     {
+            //         Console.WriteLine($"item is {item}");
+            //     }
+            //     */
+            // #endregion
+
+            // Console.WriteLine("===============");
+            // foreach (var item in myList)
+            // {
+            //     Console.WriteLine($"item is {item}");
+            // } 
             #endregion
 
-            Console.WriteLine("===============");
-            foreach (var item in myList)
-            {
-                Console.WriteLine($"item is {item}");
-            }
+
         }
     }
 }
