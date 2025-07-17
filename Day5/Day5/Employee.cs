f7ce6f1770ef84d2e0d56dd3e6c59f71a5935b49
Dev-Fady <fadymobiledev@gmail.com>
2025-07-17 16:32:13 +0300

    example for Object Initalizer

---
+++ Day5/Day5/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        // Default constructor
+        public Employee()
+        {
+            Id = 0;
+            Name = string.Empty;
+            Salary = 0;
+        }
+        public Employee(int id, string name, int salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+    }
+}
