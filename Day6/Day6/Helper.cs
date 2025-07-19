758bd126e637f1abc2d996822c577c217c37922b
Dev-Fady <fadymobiledev@gmail.com>
2025-07-19 14:58:38 +0300

    fast test

---
+++ Day6/Day6/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    internal class Helper
+    {
+        public int ID { get; set; }
+        public static bool IsOdd(int item)
+        {
+            return item % 2 != 0;
+        }
+        public  bool IsNagative(int item)
+        {
+            return item < 0;
+        }
+    }
+}
---
+++ Day6/Day6/PositiveCheckable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    internal class PositiveCheckable : ICheckable
+    {
+        public bool Check(int item)
+        {
+           return item > 0;
+        }
+    }
+}
