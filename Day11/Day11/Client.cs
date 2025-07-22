9a70886fd7f6a31bc58914414020bce958b0455d
Dev-Fady <fadymobiledev@gmail.com>
2025-07-22 17:44:03 +0300

    chat room for side server

---
+++ Day11/Day11/Client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    internal class Client
+    {
+        TcpClient tcpClient;
+        StreamReader streamReader;
+        StreamWriter streamWriter;
+
+        public Client(TcpClient tcpClient)
+        {
+            this.tcpClient = tcpClient;
+            NetworkStream stream = tcpClient.GetStream();
+            streamReader = new StreamReader(stream);
+            streamWriter = new StreamWriter(stream) { AutoFlush = true };
+        }
+
+        public async Task HandleCommunication()
+        {
+            await Send("HELLO I'm SERVER");
+
+            while (true)
+            {
+                try
+                {
+                    string msgFromClient = await streamReader.ReadLineAsync();
+                    if (msgFromClient == null) break;
+
+                    Console.WriteLine($"Client says: {msgFromClient}");
+
+                    Console.Write("Server: ");
+                    string reply = Console.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(reply))
+                        await Send(reply);
+                }
+                catch
+                {
+                    Console.WriteLine("Client disconnected.");
+                    break;
+                }
+            }
+        }
+
+        public async Task Send(string msg)
+        {
+            await streamWriter.WriteLineAsync(msg);
+        }
+    }
+}
---
+++ Day11/Day11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    internal class Program
+    {
+        static List<Client> Clients = new List<Client>();
+
+
+        static async Task Start()
+        {
+            IPAddress iPAddress = IPAddress.Parse("192.168.1.3");
+            TcpListener tcpListener = new TcpListener(iPAddress, 5500);
+            tcpListener.Start();
+            Console.WriteLine("Server started. Waiting for a connection...");
+
+            while (true)
+            {
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+                Console.WriteLine("Client connected.");
+
+                Client client = new Client(tcpClient);
+                Clients.Add(client);
+
+                _ = Task.Run(() => client.HandleCommunication()); // Start communication loop
+            }
+        }
+        static async Task Main(string[] args)
+        {
+           await Start();
+        }
+    }
+}
