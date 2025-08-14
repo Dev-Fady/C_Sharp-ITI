67f09b0ad8cb85f57fe52953e18393985de0a368
Dev-Fady <fadymobiledev@gmail.com>
2025-08-14 16:55:02 +0300

    task in C#

---
+++ task1/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+                Console.WriteLine("========================================================");
+            }
+        }
+    }
+}
